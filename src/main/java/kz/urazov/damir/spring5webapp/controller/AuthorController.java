4d382a394f42c58886ccfe8aae19de8ac1f155ea
Damir Urazov <urazovdamir@gmail.com>
2019-06-05 22:43:33 +0600

    Added controller and view for authors

---
+++ src/main/java/kz/urazov/damir/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package kz.urazov.damir.spring5webapp.controller;
+
+import kz.urazov.damir.spring5webapp.model.Author;
+import kz.urazov.damir.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
